08835fe9758c13d7726ef483f1abfadc8a717783
Erik Nguita <e.nguita@do-it.co>
2025-03-01 08:42:32 +0700

    added logs

--- redis/parse/csv-to-db-parallel.js
+++ redis/parse/csv-to-db-parallel.js
@@ -10,7 +10,7 @@ const IMPORT_FILE_NAME         = 'redis-export-redis_advabet_com-6379-2025-02-27
 // const IMPORT_FILE_NAME      = 'test';
 const IMPORT_FILE_EXTENSION    = 'csv';
 
-const WORKER_COUNT             = 60;
+const WORKER_COUNT             = 50;
 const BATCH_SIZE               = 10000;
 const MONITORING_CHECK_TIMEOUT = 5000;
 
